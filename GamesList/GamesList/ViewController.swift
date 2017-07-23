14a27bc5d47f36713f619bb77a9672ecbe0d0057
Leandro Sousa <leomesou@gmail.com>
2017-07-23 14:11:49 +0200

    Initial Commit.

---
+++ GamesList/GamesList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GamesList
+//
+//  Created by Leandro Sousa on 23/07/17.
+//  Copyright © 2017 Leandro Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
